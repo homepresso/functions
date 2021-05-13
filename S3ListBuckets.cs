5dbeb7ae7dd3708b1016641e30b530264b9fcf92
Andy Hayes <andyhayes.nz@gmail.com>
2021-05-12 20:46:53 -0600

    Added List Buckets

--- S3ListBuckets.cs
+++ S3ListBuckets.cs
@@ -7,29 +7,43 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Amazon.S3;
+using Amazon.Runtime;
+using Amazon.S3.Model;
+using System.Collections.Generic;
 
 namespace functions
 {
     public static class S3ListBuckets
     {
         [FunctionName("S3ListBuckets")]
-        public static async Task<IActionResult> Run(
+        public static async Task<List<string>> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string access = req.Headers["access"];
+            string secret = req.Headers["secret"];
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            List<string> bucketnames = new List<string>();
 
-            return new OkObjectResult(responseMessage);
+            var credentials = new BasicAWSCredentials(access, secret);
+            var config = new AmazonS3Config
+
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.USWest2
+            };
+            using var client = new AmazonS3Client(credentials, config);
+
+            ListBucketsResponse buckets = await client.ListBucketsAsync();
+            foreach (var bucket in buckets.Buckets)
+            { 
+                bucketnames.Add(bucket.BucketName);
+
+            }
+
+            return bucketnames;
         }
     }
 }
