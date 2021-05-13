9fa25d077c5478be7f6f577e2b44bc4a23573c87
Andy Hayes <andyhayes.nz@gmail.com>
2021-05-12 19:14:40 -0600

    Code Clean

---
+++ HttpTriggerCSharp1.cs
@@ -0,0 +1,68 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using Amazon;
+using Amazon.Runtime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Andys.Function
+{
+    public static class S3Upload
+    {
+        [FunctionName("S3Upload")]
+        public static async Task<String> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            var file = req.Form.Files["file"];
+            var region = req.Query["region"];
+            var access = req.Headers["access"];
+            var secret = req.Headers["secret"];
+
+
+            await UploadS3File(region, access, secret, file);
+
+
+            return  UploadS3File(region, access, secret, file).Result;
+        }
+        public static async Task<String> UploadS3File(string region, string access, string secret, IFormFile file)
+        {
+
+            var credentials = new BasicAWSCredentials(access, secret);
+            var config = new AmazonS3Config
+
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.USWest2
+            };
+            using var client = new AmazonS3Client(credentials, config);
+            await using var newMemoryStream = new MemoryStream();
+            file.CopyTo(newMemoryStream);
+
+            var uploadRequest = new TransferUtilityUploadRequest
+            {
+                InputStream = newMemoryStream,
+                Key = file.FileName,
+                BucketName = "nintexna",
+                CannedACL = S3CannedACL.PublicRead
+            };
+
+            var fileTransferUtility = new TransferUtility(client);
+            await fileTransferUtility.UploadAsync(uploadRequest);
+
+            return null;
+        }
+
+    }
+
+   
+}
+   
+
