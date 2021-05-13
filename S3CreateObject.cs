d0486b2a677c5c86996c327f3e6d124bfe126251
Andy Hayes <andyhayes.nz@gmail.com>
2021-05-12 20:46:37 -0600

    Added List Buckets

---
+++ S3CreateObject.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace functions
+{
+    public static class S3CreateObject
+    {
+        [FunctionName("S3CreateObject")]
+        public static async Task<String> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            var bucketName = req.Query["bucketName"];
+            var access = req.Headers["access"];
+            var secret = req.Headers["secret"];
+            var bodycontent = req.Form["bodycontent"];
+            var key = req.Query["key"];
+
+
+
+
+
+
+
+
+            return "NA";
+        }
+    }
+}
